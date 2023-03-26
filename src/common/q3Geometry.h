69366251211e69c8217d94c26f71d0540c2fb299
mparadinha <miguel.p.paradinha@gmail.com>
2023-03-27 00:40:14 +0100

    put some AABB methods in struct

--- src/common/q3Geometry.h
+++ src/common/q3Geometry.h
@@ -31,12 +31,37 @@ struct q3AABB {
     q3Vec3 min;
     q3Vec3 max;
 
-    bool Contains(const q3AABB& other) const;
-    bool Contains(const q3Vec3& point) const;
-    r32 SurfaceArea() const;
+    inline bool Contains(const q3AABB& other) const {
+        return min.x <= other.min.x && min.y <= other.min.y && min.z <= other.min.z &&
+               max.x >= other.max.x && max.y >= other.max.y && max.z >= other.max.z;
+    }
+
+    inline bool Contains(const q3Vec3& point) const {
+        return min.x <= point.x && min.y <= point.y && min.z <= point.z && max.x >= point.x &&
+               max.y >= point.y && max.z >= point.z;
+    }
+
+    inline r32 SurfaceArea() const {
+        r32 x = max.x - min.x;
+        r32 y = max.y - min.y;
+        r32 z = max.z - min.z;
+        return r32(2.0) * (x * y + x * z + y * z);
+    }
 };
 
-const q3AABB q3Combine(const q3AABB& a, const q3AABB& b);
+inline bool q3AABBtoAABB(const q3AABB& a, const q3AABB& b) {
+    if (a.max.x < b.min.x || a.min.x > b.max.x) return false;
+    if (a.max.y < b.min.y || a.min.y > b.max.y) return false;
+    if (a.max.z < b.min.z || a.min.z > b.max.z) return false;
+    return true;
+}
+
+inline const q3AABB q3Combine(const q3AABB& a, const q3AABB& b) {
+    return q3AABB{
+        .min = q3Min(a.min, b.min),
+        .max = q3Max(a.max, b.max),
+    };
+}
 
 struct q3HalfSpace {
     q3HalfSpace();
@@ -60,70 +85,23 @@ struct q3RaycastData {
     r32 toi;       // Solved time of impact
     q3Vec3 normal; // Surface normal at impact
 
-    void Set(const q3Vec3& startPoint, const q3Vec3& direction, r32 endPointTime);
+    inline void Set(const q3Vec3& startPoint, const q3Vec3& direction, r32 endPointTime) {
+        start = startPoint;
+        dir = direction;
+        t = endPointTime;
+    }
 
-    // Uses toi, start and dir to compute the point at toi. Should
-    // only be called after a raycast has been conducted with a
-    // return value of true.
-    const q3Vec3 GetImpactPoint() const;
+    // Uses toi, start and dir to compute the point at toi. Should only be called after
+    // a raycast has been conducted with a return value of true.
+    inline const q3Vec3 GetImpactPoint() const { return q3Vec3(start + dir * toi); }
 };
 
 inline void q3ComputeBasis(const q3Vec3& a, q3Vec3* __restrict b, q3Vec3* __restrict c) {
-
-    if (q3Abs(a.x) >= r32(0.57735027))
+    if (q3Abs(a.x) >= r32(0.57735027)) {
         b->Set(a.y, -a.x, r32(0.0));
-    else
+    } else {
         b->Set(r32(0.0), a.z, -a.y);
-
+    }
     *b = q3Normalize(*b);
     *c = q3Cross(a, *b);
-}
-
-inline bool q3AABBtoAABB(const q3AABB& a, const q3AABB& b) {
-    if (a.max.x < b.min.x || a.min.x > b.max.x) return false;
-
-    if (a.max.y < b.min.y || a.min.y > b.max.y) return false;
-
-    if (a.max.z < b.min.z || a.min.z > b.max.z) return false;
-
-    return true;
-}
-
-inline bool q3AABB::Contains(const q3AABB& other) const {
-    return min.x <= other.min.x && min.y <= other.min.y && min.z <= other.min.z &&
-           max.x >= other.max.x && max.y >= other.max.y && max.z >= other.max.z;
-}
-
-inline bool q3AABB::Contains(const q3Vec3& point) const {
-    return min.x <= point.x && min.y <= point.y && min.z <= point.z && max.x >= point.x &&
-           max.y >= point.y && max.z >= point.z;
-}
-
-inline r32 q3AABB::SurfaceArea() const {
-    r32 x = max.x - min.x;
-    r32 y = max.y - min.y;
-    r32 z = max.z - min.z;
-
-    return r32(2.0) * (x * y + x * z + y * z);
-}
-
-inline const q3AABB q3Combine(const q3AABB& a, const q3AABB& b) {
-    q3AABB c;
-
-    c.min = q3Min(a.min, b.min);
-    c.max = q3Max(a.max, b.max);
-
-    return c;
-}
-
-inline void q3RaycastData::Set(
-    const q3Vec3& startPoint, const q3Vec3& direction, r32 endPointTime
-) {
-    start = startPoint;
-    dir = direction;
-    t = endPointTime;
-}
-
-inline const q3Vec3 q3RaycastData::GetImpactPoint() const {
-    return q3Vec3(start + dir * toi);
 }
